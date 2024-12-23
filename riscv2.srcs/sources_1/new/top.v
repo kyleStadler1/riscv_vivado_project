e392f99cd56a5d08893d22072809a6edb8e45ba7
kyleStadler1 <kcs-github@stadfam.net>
2024-12-23 01:03:26 +0000

    vivado proj setup

---
+++ riscv2.srcs/sources_1/new/top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/23/2024 12:58:10 AM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+
+    );
+endmodule
