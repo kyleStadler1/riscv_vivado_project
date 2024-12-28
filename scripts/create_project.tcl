05f92ffb6e08d093ea22f39eb66fbac7a81dd842
Kyle Stadler <kstadler@Mac.localdomain>
2024-12-28 14:31:04 -0800

    tcl try again

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -19,9 +19,14 @@ add_files ../src/top.v
 
 # Add IP core
 read_ip ../ips/DualBRAM3.xci
-generate_target {synthesis simulation} [get_files ../ips/DualBRAM3.xci]
-# Generate IP output products (required for new environments)
-generate_target all [get_ips]
+
+# Set custom output directories for the IP core
+set_property GENERATE_SIMULATION_OUTPUT_DIRECTORY $project_dir/ip_generated/sim [get_files ../ips/DualBRAM3.xci]
+set_property GENERATE_SYNTHESIS_OUTPUT_DIRECTORY $project_dir/ip_generated/synth [get_files ../ips/DualBRAM3.xci]
+set_property GENERATE_IMPLEMENTATION_OUTPUT_DIRECTORY $project_dir/ip_generated/impl [get_files ../ips/DualBRAM3.xci]
+
+# Generate IP output products
+generate_target all [get_files ../ips/DualBRAM3.xci]
 
 # Add constraints file
 add_files -fileset constrs_1 ../constraints/constraints.xdc
@@ -29,8 +34,53 @@ add_files -fileset constrs_1 ../constraints/constraints.xdc
 # Set the top module to 'top'
 set_property top top [current_fileset]
 
+# Save the project
+save_project
 
-
-# Save and close the project
-save_project_as $project_name
+# Close the project
 close_project
+
+
+
+
+
+
+
+
+
+# # Define project parameters
+# set project_name "riscv2"
+# set project_dir "../riscv2_project"
+
+# # Clean up previous project directory
+# file delete -force $project_dir
+
+# # Create a new Vivado project
+# create_project $project_name $project_dir -part xc7z007sclg400-1
+
+# # Add source files
+# add_files ../src/PC.v
+# add_files ../src/ModeFSM.v
+# add_files ../src/MemIO.v
+# add_files ../src/InstrInject.v
+# add_files ../src/FetchLatch.v
+# add_files ../src/bramtest.v
+# add_files ../src/top.v
+
+# # Add IP core
+# read_ip ../ips/DualBRAM3.xci
+# generate_target {synthesis simulation} [get_files ../ips/DualBRAM3.xci]
+# # Generate IP output products (required for new environments)
+# generate_target all [get_ips]
+
+# # Add constraints file
+# add_files -fileset constrs_1 ../constraints/constraints.xdc
+
+# # Set the top module to 'top'
+# set_property top top [current_fileset]
+
+
+
+# # Save and close the project
+# save_project_as $project_name
+# close_project
